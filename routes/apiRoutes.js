2c2a5a2f7805be5f4b9f3d3b466aec378e7f88f4
pindellk <pindellk@protonmail.com>
2020-12-21 23:34:25 -0500

    Update folder structure and related paths, add server.js - front end works

---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+// Require model class(es)? 
+
+
+// module.exports = function (app) {
+
+//     // GET request to display notes
+//     app.get("/api/workouts", );
+
+//     // POST request to add note
+//     app.post("/api/workouts", );
+
+//     // DELETE note based on id
+//     app.delete("/api/workouts/:id", );
+// };
---
+++ server.js
@@ -0,0 +1,19 @@
+// Establish dependencies
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Body parser middleware express app to handle data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+// Point our server to specific routes
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Server listener
+app.listen(PORT, () => {
+    console.log("App listening on PORT: " + PORT)
+});
